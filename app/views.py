7d5b0d77cd50e404120028973554e0800954c344
Steve Prosser <sprosser@opinionlab.com>
2015-05-22 15:29:16 -0500

    Basic routing and templating

--- app/views.py
+++ app/views.py
@@ -1,7 +1,23 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'name': 'Steve'} # fake user
+    teams = [
+        {
+            'team_captain': {'name': 'Joe Mama'},
+            'members': 'Joe, Jim, and Jack'
+        },
+        {
+            'team_captain': {'name': "Mr. Sea Otter"},
+            'members': 'Mr. Sea Otter, Mrs. Sea Otter, and Nigel'
+        }
+            ]
+    return render_template('index.html',
+                            title='Home',
+                            user=user,
+                            teams=teams)
+
